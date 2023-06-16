1de119c5c6a7391fd3454f9885341e3950683ad6
Safni Nazar <109753567+shafni50@users.noreply.github.com>
2023-06-16 18:07:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-function submitButton() {
-    alert('Submitted!')
+let newSub1 = 0;
+
+//how many has Subscribed
+function newSub(){
+  newSub1 ++; // same to newSub + 1
+  console.log( `Got a subscriber! : ${newSub1}`)
 };
+
+//Subscribed
+function submitted(){
+  const buttonElement = document.querySelector('.sub-button')
+
+  if (buttonElement.innerText === 'SUBSCRIBE') {
+    buttonElement.innerHTML = 'Subscribed'.toUpperCase()
+  } else {
+    buttonElement.innerHTML = 'SUBSCRIBE'
+  }
+};
+
